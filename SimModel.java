027031fe8bb785a583019bbcd61c02b7bd3e7cb9
liustone99 <79604926+liustone99@users.noreply.github.com>
2021-02-27 18:53:59 -0500

    Update SimModel.java
    Comments added, readFile function added (may need to be modified

--- SimModel.java
+++ SimModel.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+
 public class SimModel {
 
     //Sim Model Variables
@@ -17,21 +20,41 @@ public class SimModel {
 
     private static double[] readFile(String fileName) {
         // reads from given data files and returns an array
+        double[] fileContents = new double[300]; //Double array to store values read from file
+        int i = 0;
+        int data;
+        try{
+            File myFile = new File(fileName);
+            Scanner myReader = new Scanner(myFile);
+            while(myReader.hasNextLine()){
+                fileContents[i] = Double.parseDouble(myReader.nextLine());
+                i+=1;
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+        return fileContents;
+
     }
 
     private static void initialization(){
-        // initializes the
+        // initializes the values for the variables declared, creates instances of inspector and workstation
     }
 
     public static void main(String[] args) {
-
+        // Initializes the FEL and queues. Calls the various functions defined
+       
     }
 
     private static void GenerateReport(){
-
+        // For output
     }
 
     private static void checkMinutes(double seconds) {
+        //updates the amount of minutes passed
         if (seconds == 60){
             minutes += 1;
             seconds = 0;
@@ -39,6 +62,7 @@ public class SimModel {
     }
 
     private static void checkHour(double minutes) {
+        //updates the amount of hours passed
         if (minutes == 60){
             hours += 1;
             minutes = 0;
@@ -46,6 +70,7 @@ public class SimModel {
     }
 
     private static void checkDay(double hours) {
+        //Updates the amount of days passed
         if (hours == 24){
             currentDay += 1;
             hours = 0;
@@ -53,23 +78,23 @@ public class SimModel {
     }
 
     private static void ProcessInspector(SimEvent evt){
-
+    //Inspector process
     }
 
     private static void ProcessWorkstation(SimEvent evt){
-
+    //Workstation process
     }
 
     private static void ScheduleEventInspector(int index, double[] process_time_array, SimEvent.eventType eType, inspector ins){
-
+    //Event for inspector
     }
 
     private static void ScheduleEventWorkstation(int index, double[] process_time_array, SimEvent.eventType eType, workstation ws){
-
+    // Event for workstation
     }
 
     private static void ProcessSimEvent(SimEvent nxtEvent){
-
+        //Process the simulation event
     }
 
 }
